8188f3e51e20bef51ab04235f09af554ffdadeef
Heng Layhak <layhakheng@gmail.com>
2024-06-11 23:06:20 +0700

    change user endpoint to use username and also can sending static mail to real gmail acc

--- src/main/java/co/istad/dealkh/exception/GlobalRestControllerAdviser.java
+++ src/main/java/co/istad/dealkh/exception/GlobalRestControllerAdviser.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,19 +17,6 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalRestControllerAdviser {
 
-    @ExceptionHandler(SingleRoleException.class)
-    public ResponseEntity<BasedErrorResponse<String>> handleSingleRoleException(SingleRoleException ex) {
-        BasedError<String> basedError = BasedError.<String>builder()
-                .code(HttpStatus.BAD_REQUEST.toString())
-                .description(ex.getReason())
-                .build();
-
-        BasedErrorResponse<String> basedErrorResponse = BasedErrorResponse.<String>builder()
-                .error(basedError)
-                .build();
-
-        return new ResponseEntity<>(basedErrorResponse, HttpStatus.BAD_REQUEST);
-    }
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<BasedErrorResponse<List<Map<String, Object>>>> handleCustomException(CustomException ex) {
@@ -45,35 +31,6 @@ public class GlobalRestControllerAdviser {
 
         return ResponseEntity.status(ex.getStatusCode()).body(basedErrorResponse);
     }
-
-    @ExceptionHandler(HttpClientErrorException.Unauthorized.class)
-    public ResponseEntity<BasedErrorResponse<String>> handleUnauthorizedException(HttpClientErrorException.Unauthorized ex) {
-        BasedError<String> basedError = BasedError.<String>builder()
-                .code(HttpStatus.UNAUTHORIZED.toString())
-                .description(ex.getResponseBodyAsString())
-                .build();
-
-        BasedErrorResponse<String> basedErrorResponse = BasedErrorResponse.<String>builder()
-                .error(basedError)
-                .build();
-
-        return new ResponseEntity<>(basedErrorResponse, HttpStatus.UNAUTHORIZED);
-    }
-
-    @ExceptionHandler(HttpClientErrorException.Forbidden.class)
-    public ResponseEntity<BasedErrorResponse<String>> handleForbiddenException(HttpClientErrorException.Forbidden ex) {
-        BasedError<String> basedError = BasedError.<String>builder()
-                .code(HttpStatus.FORBIDDEN.toString())
-                .description(ex.getResponseBodyAsString())
-                .build();
-
-        BasedErrorResponse<String> basedErrorResponse = BasedErrorResponse.<String>builder()
-                .error(basedError)
-                .build();
-
-        return new ResponseEntity<>(basedErrorResponse, HttpStatus.FORBIDDEN);
-    }
-
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
