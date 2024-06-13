e7d521d3fea359ce169d8ff6844181a05cb97616
Heng Layhak <layhakheng@gmail.com>
2024-06-13 17:09:09 +0700

    remove id from the response

--- src/main/java/co/istad/dealkh/features/shop/dto/ShopResponse.java
+++ src/main/java/co/istad/dealkh/features/shop/dto/ShopResponse.java
@@ -26,7 +26,7 @@ import java.util.List;
  * @param location
  */
 public record ShopResponse(
-        Long id,
+//        Long id,
         String name,
         String slug,
         String address,
